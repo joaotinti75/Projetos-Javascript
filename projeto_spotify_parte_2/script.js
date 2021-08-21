f0711f8fe4fa96b02540126a1ecc7b44cfa5d0df
Joao Tinti <joaotinti75@gmail.com>
2021-08-21 10:24:03 -0300

    projeto 2 finalizado com sucesso

--- projeto_spotify_parte_2/script.js
+++ projeto_spotify_parte_2/script.js
@@ -1,25 +1,20 @@
 let musicas = [
-    {titulo:'Guitar solo', artista:'João Tinti', source:'musicas/We Ride! - Reed Mathis.mp3', img:'imagens/rock.jpg'},
-    {titulo:'Samba raiz', artista:'Bossa Nova Brasil', source:'musicas/Ella Vater - The Mini Vandals.mp3', img:'imagens/samba.jpg'},
-    {titulo:'Música piano', artista:'John Green', source:'musicas/A Brand New Start - TrackTribe (1).mp3', img:'imagens/piano.jpg'}
+    {titulo:'Guitar solo', artista:'João Tinti', src:'musicas/We Ride! - Reed Mathis.mp3', img:'imagens/rock.jpg'},
+    {titulo:'Samba raiz', artista:'Bossa Nova Brasil', src:'musicas/Ella Vater - The Mini Vandals.mp3', img:'imagens/samba.jpg'},
+    {titulo:'Música piano', artista:'John Green', src:'musicas/A Brand New Start - TrackTribe (1).mp3', img:'imagens/piano.jpg'}
 ];
 
-// INICIO
 let musica = document.querySelector('audio');
-let musicaIndex = 0;
+let indexMusica = 0;
 
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
 let nomeMusica = document.querySelector('.descricao h2');
 let nomeArtista = document.querySelector('.descricao i');
-let imagem = document.querySelector('img');
-let tempoDecorrido = document.querySelector('.tempo .inicio');
-let duracaoMusica = document.querySelector('.tempo .fim');
 
-nomeMusica.textContent = musicas[musicaIndex].titulo;
-nomeArtista.textContent = musicas[musicaIndex].artista;
-imagem.setAttribute('src', musicas[musicaIndex].img);
-duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+renderizarMusica(indexMusica);
 
-// EVENTOS
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
@@ -27,61 +22,58 @@ document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 musica.addEventListener('timeupdate', atualizarBarra);
 
 document.querySelector('.anterior').addEventListener('click', () => {
-    musicaIndex--; 
-    if (musicaIndex < 0){
-        musicaIndex = 2;
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
     }
-    renderizarMusica(musicaIndex);
+    renderizarMusica(indexMusica);
 });
 
-document.querySelector('.proximo').addEventListener('click', () => {
-    musicaIndex++;
-    if (musicaIndex > 2){
-        musicaIndex = 0;
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
     }
-    renderizarMusica(musicaIndex);
+    renderizarMusica(indexMusica);
 });
 
-// FUNÇÕES
-
-function renderizarMusica(musicaIndex){
-    musica.setAttribute('src', musicas[musicaIndex].source);
-
+// Funções
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
     musica.addEventListener('loadeddata', () => {
-        nomeMusica.textContent = musicas[musicaIndex].titulo;
-        nomeArtista.textContent = musicas[musicaIndex].artista;
-        imagem.src = musicas[musicaIndex].img;
-    
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
         duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
     });
-
-    document.body.append(musica);
 }
 
 function tocarMusica(){
     musica.play();
-    document.querySelector('.botao-play').style.display = 'none';
     document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
 
 function pausarMusica(){
     musica.pause();
-    document.querySelector('.botao-play').style.display = 'block';
     document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
 function segundosParaMinutos(segundos){
     let campoMinutos = Math.floor(segundos / 60);
     let campoSegundos = segundos % 60;
-
     if (campoSegundos < 10){
-        campoSegundos = '0'+ campoSegundos;
+        campoSegundos = '0' + campoSegundos;
     }
-    return `${campoMinutos}:${campoSegundos}`;
+
+    return campoMinutos+':'+campoSegundos;
 }
 
-function atualizarBarra(){
-    let barra = document.querySelector('progress');
-    barra.style.width = Math.floor((musica.currentTime / musica.duration)*100) + '%';
-    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
-}
