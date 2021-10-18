10e3ff02654620303b7de4670340094b88c9e031
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2021-10-17 23:43:14 -0300

    Create script.js

---
+++ Geolocalização/script.js
@@ -0,0 +1,17 @@
+let h2 = document.querySelector('h2');
+
+function success(pos){
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+}
+
+function error(err){
+    console.log(err);
+}
+
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
+
+//navigator.geolocation.clearWatch(watchID);
