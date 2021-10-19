afefe4ed334e3a5a2285a5825028adbf0a981aca
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2021-10-19 10:34:41 -0300

    Create script.js

---
+++ Leaflet/script.js
@@ -0,0 +1,36 @@
+
+let h2 = document.querySelector('h2');
+var map;
+
+function success(pos){
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+
+    if (map === undefined) {
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+    } else {
+        map.remove();
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+    }
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+        .bindPopup('Eu estou aqui!')
+        .openPopup();
+}
+
+function error(err){
+    console.log(err);
+}
+
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
+
+//navigator.geolocation.clearWatch(watchID);
+
+
