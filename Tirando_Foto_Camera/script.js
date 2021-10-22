4857077895313dc8f32a77066a48b5f11f0f85f7
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2021-10-21 21:39:42 -0300

    Create script.js

---
+++ Tirando_Foto_Camera/script.js
@@ -0,0 +1,23 @@
+var video = document.querySelector('video');
+
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream => {
+    video.srcObject = stream;
+    video.play();
+})
+.catch(error => {
+    console.log(error);
+})
+
+document.querySelector('button').addEventListener('click', () => {
+    var canvas = document.querySelector('canvas');
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+    var link = document.createElement('a');
+    link.download = 'foto.png';
+    link.href = canvas.toDataURL();
+    link.textContent = 'Clique para baixar a imagem';
+    document.body.appendChild(link);
+});
